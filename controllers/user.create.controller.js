f62b4d6bb34f7e6b0618e55acd1b9a8ed290512d
Muskan <muskan4mann@gmail.com>
2026-01-20 01:18:26 +0530

    till 21.1.26 0117 hrs

--- controllers/user.create.controller.js
+++ controllers/user.create.controller.js
@@ -7,16 +7,28 @@ async function user_create(pwd,mail) {
         // designation:'Student'
         // })
 
-        const user=await User.create({
+        const user=await User.insertOne({
         email:mail,
         // email:mail,
         password:pwd,
         quizzes_created:[]
         // designation:desig
         })
+
+//         const newUser = new User({
+//   email: req.body.username,
+//   password: req.body.password
+//   });
+//   newUser.save(function(err){
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.render("secrets");
+//    }
+//  });
         // console.log("in user creation fnctn")
-        console.log(user)
-        return user
+        console.log(newUser)
+        return newUser
         
     } catch (error) {
         console.log("usermodel err:",error)
--- models/users.model.js
+++ models/users.model.js
@@ -2,12 +2,14 @@ const {model,Schema} =require('mongoose')
 // const{Quiz}=require('./quizzes.model')
 // const mongoose=require("mongoose")
 const usertable_schema=new Schema({
-    email:String,password:String,
+    email:String,
+    password:String,
+
     // Designation:String,
     quizzes_created:[{
         type:Schema.Types.ObjectId, 
         ref:'Quiz'
     }]
 })
-const User =model('User',usertable_schema)
+const User =model('user',usertable_schema)
 module.exports={User};
