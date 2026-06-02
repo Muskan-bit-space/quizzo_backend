6dbb47659b1d24d8f438324ba94a7a4ee8b077ab
Muskan <muskan4mann@gmail.com>
2026-06-02 18:54:19 +0530

    chl gya kuchh toh

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
 const router=express.Router();  //-----this is like app 
-const {signup}=require('./controllers/auth.controller.js')
+const {signup}=require('../controllers/auth.controller.js')
 router.post('/signup',signup)
 module.exports=router
